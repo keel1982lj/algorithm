3b729dfbf6107a02e81c501be6f053ef37e44d4c
keel1982lj <keel1982@outlook.com>
2019-01-18 16:17:54 +0800

    Initial commit

---
+++ alg_solution_file/alg_main/Rec_Class.cpp
@@ -0,0 +1,79 @@
+#include "stdafx.h"
+#include "Rec_Class.h"
+#include <iostream>
+
+int Rec_Class::count = 0;
+
+Rec_Class::Rec_Class()
+{
+}
+
+
+Rec_Class::~Rec_Class()
+{
+}
+
+//递归计算阶乘
+int Rec_Class::factorial(int n) {
+	if (n == 0)return 1;
+	else return n * factorial(n - 1);
+}
+
+//基于递归的插入排序算法
+void Rec_Class::insert_sort_rec(int A[], int n) {
+	if (n > 1) {
+		insert_sort_rec(A, n - 1);
+		int temp = A[n - 1];
+		for (int i = n - 2; i >= 0; i--) {
+			if (A[i] > temp) {
+				A[i + 1] = A[i];
+				if (i == 0) A[i] = temp;
+			}
+			else {
+				A[i + 1] = temp;
+				break;
+			}
+		}
+	}
+}
+
+//递归计算整数幂
+int Rec_Class::power(int x, int n) {
+	if (n == 1)
+		return x;
+	else
+		return power(x, n / 2)*power(x, n - n / 2);
+}
+
+//多项式求值的递归算法
+float Rec_Class::horner_pol(float x, float A[], int n)
+{
+	if (n != 1) {
+		return horner_pol(x, A, n - 1)*x + A[n-1];
+	}
+	else {
+		return A[0];
+	}
+}
+
+void swap(int *A,int n, int m) {
+	int temp = A[n];
+	A[n] = A[m];
+	A[m] = temp;
+}
+//排列的生成递归算法
+void Rec_Class::perm(int A[], int k, int n)
+{
+	if (k > 1) {
+		perm(A, k - 1, n);
+		for (int i = k - 1; i > 0; i--) {
+			swap(A, i, i - 1);
+			for (int j = 0; j < n; j++) std::cout << A[j];
+			std::cout << std::endl; count++;
+		}
+	}
+	else if (k == 1) {
+		for (int j = 0; j < n; j++) std::cout << A[j];
+		std::cout << std::endl; count++;
+	}
+}
---
+++ alg_solution_file/alg_main/Rec_Class.h
@@ -0,0 +1,14 @@
+#pragma once
+class Rec_Class
+{
+public:
+	Rec_Class();
+	~Rec_Class();
+	int factorial(int n);
+	static void insert_sort_rec(int A[], int n);//递归插入排序 （n^2） （n）
+	static int power(int x, int n);//递归阶乘（log n）（log n）
+	static float horner_pol(float x, float A[], int n);//多项式求值的递归算法 (n) (n)
+	static void perm(int A[], int k, int n);//排列问题的递归算法，k为当前递归层次需完成排列的元素个数
+	static int count;//排列问题中用的计数器
+};
+
